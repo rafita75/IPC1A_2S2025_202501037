f69061790e435e52a67d2770c4d3c899172b161d
Angel Juarez <estudiosjuarez24@gmail.com>
2026-03-19 18:33:20 -0600

    Vamos muy lentos con las interfaces, pero la logica ya la tenemos en mente y hare uso de IA para ayudarme, ya que no tengo mucho tiempo, Interfaz Grafica de Registro

--- Proyecto1/src/views/Registro.java
+++ Proyecto1/src/views/Registro.java
@@ -2,12 +2,12 @@ package views;
 
 import java.awt.Color;
 
-public class Login extends javax.swing.JFrame {
+public class Registro extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Login.class.getName());
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Registro.class.getName());
     int xMouse, yMouse;
     
-    public Login() {
+    public Registro() {
         initComponents();
     }
 
@@ -31,8 +31,12 @@ public class Login extends javax.swing.JFrame {
         tituloIngresar = new javax.swing.JLabel();
         BarraNavegacion = new javax.swing.JPanel();
         btnExit = new javax.swing.JLabel();
-        crearCuenta = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        tituloCarnet = new javax.swing.JLabel();
+        jSeparator3 = new javax.swing.JSeparator();
+        txtCarnet = new javax.swing.JTextField();
+        tituloCarrera = new javax.swing.JLabel();
+        jSeparator4 = new javax.swing.JSeparator();
+        txtCarrera = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
@@ -60,7 +64,7 @@ public class Login extends javax.swing.JFrame {
         bgImagen.setLayout(bgImagenLayout);
         bgImagenLayout.setHorizontalGroup(
             bgImagenLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(LogoUsac, javax.swing.GroupLayout.DEFAULT_SIZE, 370, Short.MAX_VALUE)
+            .addComponent(LogoUsac, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(NombreProyecto, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(NombreFacultad, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
@@ -78,16 +82,16 @@ public class Login extends javax.swing.JFrame {
         bg.add(bgImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 0, 370, 600));
 
         Titulo.setFont(new java.awt.Font("Stencil", 1, 36)); // NOI18N
-        Titulo.setText("Iniciar Sesión");
-        bg.add(Titulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 90, 300, 90));
+        Titulo.setText("Crear Cuenta");
+        bg.add(Titulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 40, 300, 90));
 
         tituloContraseña.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
         tituloContraseña.setText("COntraseña");
-        bg.add(tituloContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 290, 200, 40));
+        bg.add(tituloContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 220, 200, 40));
 
         tituloUsuario.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
         tituloUsuario.setText("Usuario");
-        bg.add(tituloUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 180, 200, 40));
+        bg.add(tituloUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 130, 200, 40));
 
         txtUsuario.setFont(new java.awt.Font("Stencil", 0, 12)); // NOI18N
         txtUsuario.setForeground(new java.awt.Color(204, 204, 204));
@@ -98,7 +102,7 @@ public class Login extends javax.swing.JFrame {
                 txtUsuarioMousePressed(evt);
             }
         });
-        bg.add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 230, 400, 40));
+        bg.add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 170, 400, 40));
 
         txtContra.setFont(new java.awt.Font("Times New Roman", 0, 12)); // NOI18N
         txtContra.setForeground(new java.awt.Color(204, 204, 204));
@@ -109,9 +113,9 @@ public class Login extends javax.swing.JFrame {
                 txtContraMousePressed(evt);
             }
         });
-        bg.add(txtContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 340, 400, 40));
-        bg.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 380, 400, -1));
-        bg.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 270, 400, -1));
+        bg.add(txtContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 260, 400, 40));
+        bg.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 300, 400, -1));
+        bg.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 210, 400, -1));
 
         btnIngresar.setBackground(new java.awt.Color(204, 204, 204));
 
@@ -132,18 +136,24 @@ public class Login extends javax.swing.JFrame {
         btnIngresar.setLayout(btnIngresarLayout);
         btnIngresarLayout.setHorizontalGroup(
             btnIngresarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(btnIngresarLayout.createSequentialGroup()
-                .addComponent(tituloIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addGap(0, 150, Short.MAX_VALUE)
+            .addGroup(btnIngresarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(btnIngresarLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(tituloIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         btnIngresarLayout.setVerticalGroup(
             btnIngresarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, btnIngresarLayout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(tituloIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGap(0, 50, Short.MAX_VALUE)
+            .addGroup(btnIngresarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(btnIngresarLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(tituloIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
 
-        bg.add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 420, 150, 50));
+        bg.add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 510, 150, 50));
 
         BarraNavegacion.setBackground(new java.awt.Color(255, 255, 255));
         BarraNavegacion.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
@@ -179,7 +189,7 @@ public class Login extends javax.swing.JFrame {
             BarraNavegacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(BarraNavegacionLayout.createSequentialGroup()
                 .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 865, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         BarraNavegacionLayout.setVerticalGroup(
             BarraNavegacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -188,34 +198,38 @@ public class Login extends javax.swing.JFrame {
 
         bg.add(BarraNavegacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, 40));
 
-        crearCuenta.setBackground(new java.awt.Color(255, 255, 255));
-        crearCuenta.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                crearCuentaMouseClicked(evt);
+        tituloCarnet.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
+        tituloCarnet.setText("Carnet");
+        bg.add(tituloCarnet, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 310, 200, 40));
+        bg.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 390, 400, -1));
+
+        txtCarnet.setFont(new java.awt.Font("Stencil", 0, 12)); // NOI18N
+        txtCarnet.setForeground(new java.awt.Color(204, 204, 204));
+        txtCarnet.setText("Ingrese su numero de carnet");
+        txtCarnet.setBorder(null);
+        txtCarnet.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                txtCarnetMousePressed(evt);
             }
         });
+        bg.add(txtCarnet, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 350, 400, 40));
 
-        jLabel1.setBackground(new java.awt.Color(0, 51, 255));
-        jLabel1.setFont(new java.awt.Font("Sitka Text", 0, 10)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 255));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("¿Aún no tienes cuenta? Crea una cuenta aquí");
+        tituloCarrera.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
+        tituloCarrera.setText("Carrera");
+        bg.add(tituloCarrera, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 400, 200, 40));
+        bg.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 480, 400, -1));
 
-        javax.swing.GroupLayout crearCuentaLayout = new javax.swing.GroupLayout(crearCuenta);
-        crearCuenta.setLayout(crearCuentaLayout);
-        crearCuentaLayout.setHorizontalGroup(
-            crearCuentaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
-        );
-        crearCuentaLayout.setVerticalGroup(
-            crearCuentaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(crearCuentaLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        bg.add(crearCuenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 480, 270, 30));
+        txtCarrera.setFont(new java.awt.Font("Stencil", 0, 12)); // NOI18N
+        txtCarrera.setForeground(new java.awt.Color(204, 204, 204));
+        txtCarrera.setText("Ingrese su carrera");
+        txtCarrera.setBorder(null);
+        txtCarrera.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                txtCarreraMousePressed(evt);
+            }
+        });
+        txtCarrera.addActionListener(this::txtCarreraActionPerformed);
+        bg.add(txtCarrera, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 440, 400, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -271,6 +285,14 @@ public class Login extends javax.swing.JFrame {
             txtContra.setText("**********");
             txtContra.setForeground(new Color(204, 204, 204));
         }
+        if(txtCarrera.getText().isEmpty()){
+            txtCarrera.setText("Ingrese su carrera");
+            txtCarrera.setForeground(new Color(204, 204, 204));
+        }
+        if(txtCarnet.getText().isEmpty()){
+            txtCarnet.setText("Ingrese su numero de carnet");
+            txtCarnet.setForeground(new Color(204, 204, 204));
+        }
     }//GEN-LAST:event_txtUsuarioMousePressed
 
     private void txtContraMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtContraMousePressed
@@ -282,13 +304,57 @@ public class Login extends javax.swing.JFrame {
             txtUsuario.setText("Ingrese su nombre de usuario");
             txtUsuario.setForeground(new Color(204, 204, 204));
         }
+        if(txtCarrera.getText().isEmpty()){
+            txtCarrera.setText("Ingrese su carrera");
+            txtCarrera.setForeground(new Color(204, 204, 204));
+        }
+        if(txtCarnet.getText().isEmpty()){
+            txtCarnet.setText("Ingrese su numero de carnet");
+            txtCarnet.setForeground(new Color(204, 204, 204));
+        }
     }//GEN-LAST:event_txtContraMousePressed
 
-    private void crearCuentaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_crearCuentaMouseClicked
-        Registro mostrar = new Registro();
-        mostrar.show();
-        dispose();
-    }//GEN-LAST:event_crearCuentaMouseClicked
+    private void txtCarnetMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtCarnetMousePressed
+        if(txtCarnet.getText().equals("Ingrese su numero de carnet")){
+            txtCarnet.setText("");
+            txtCarnet.setForeground(Color.black);
+        }
+        if(txtUsuario.getText().isEmpty()){
+            txtUsuario.setText("Ingrese su nombre de usuario");
+            txtUsuario.setForeground(new Color(204, 204, 204));
+        }
+        if(txtCarrera.getText().isEmpty()){
+            txtCarrera.setText("Ingrese su carrera");
+            txtCarrera.setForeground(new Color(204, 204, 204));
+        }
+        if(String.valueOf(txtContra.getPassword()).isEmpty()){
+            txtContra.setText("**********");
+            txtContra.setForeground(new Color(204, 204, 204));
+        }
+    }//GEN-LAST:event_txtCarnetMousePressed
+
+    private void txtCarreraMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtCarreraMousePressed
+        if(txtCarrera.getText().equals("Ingrese su carrera")){
+            txtCarrera.setText("");
+            txtCarrera.setForeground(Color.black);
+        }
+        if(String.valueOf(txtContra.getPassword()).isEmpty()){
+            txtContra.setText("**********");
+            txtContra.setForeground(new Color(204, 204, 204));
+        }
+        if(txtUsuario.getText().isEmpty()){
+            txtUsuario.setText("Ingrese su nombre de usuario");
+            txtUsuario.setForeground(new Color(204, 204, 204));
+        }
+        if(txtCarnet.getText().isEmpty()){
+            txtCarnet.setText("Ingrese su numero de carnet");
+            txtCarnet.setForeground(new Color(204, 204, 204));
+        }
+    }//GEN-LAST:event_txtCarreraMousePressed
+
+    private void txtCarreraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCarreraActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtCarreraActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -309,7 +375,7 @@ public class Login extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new Login().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new Registro().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -322,13 +388,17 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JPanel bgImagen;
     private javax.swing.JLabel btnExit;
     private javax.swing.JPanel btnIngresar;
-    private javax.swing.JPanel crearCuenta;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JSeparator jSeparator4;
+    private javax.swing.JLabel tituloCarnet;
+    private javax.swing.JLabel tituloCarrera;
     private javax.swing.JLabel tituloContraseña;
     private javax.swing.JLabel tituloIngresar;
     private javax.swing.JLabel tituloUsuario;
+    private javax.swing.JTextField txtCarnet;
+    private javax.swing.JTextField txtCarrera;
     private javax.swing.JPasswordField txtContra;
     private javax.swing.JTextField txtUsuario;
     // End of variables declaration//GEN-END:variables
